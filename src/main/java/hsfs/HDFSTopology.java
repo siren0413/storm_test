df7ce7055a7d30f7c564192a443f6fb431a3ad08
yijunmao <siren0413@gmail.com>
2017-07-12 00:42:01 -0700

    remote storm cluster

--- src/main/java/hsfs/HDFSTopology.java
+++ src/main/java/hsfs/HDFSTopology.java
@@ -1,25 +1,28 @@
 package hsfs;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
 import org.apache.storm.hdfs.bolt.HdfsBolt;
 import org.apache.storm.hdfs.bolt.format.DefaultFileNameFormat;
 import org.apache.storm.hdfs.bolt.format.DelimitedRecordFormat;
 import org.apache.storm.hdfs.bolt.format.FileNameFormat;
 import org.apache.storm.hdfs.bolt.format.RecordFormat;
 import org.apache.storm.hdfs.bolt.rotation.FileRotationPolicy;
-import org.apache.storm.hdfs.bolt.rotation.TimedRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.FileSizeRotationPolicy;
 import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
 import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
 import org.apache.storm.hdfs.common.rotation.MoveFileAction;
-import org.apache.storm.hdfs.spout.HdfsSpout;
+import org.apache.storm.kafka.spout.Func;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
 import org.apache.storm.topology.TopologyBuilder;
-import org.yaml.snakeyaml.Yaml;
+import org.apache.storm.tuple.Fields;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.storm.kafka.spout.KafkaSpoutConfig.FirstPollOffsetStrategy.UNCOMMITTED_LATEST;
 
 /**
  * Created by yijunmao on 7/11/17.
@@ -28,28 +31,30 @@ public class HDFSTopology {
     public static void main(String[] args) throws Exception {
         SyncPolicy syncPolicy = new CountSyncPolicy(1000);
 
-        FileRotationPolicy rotationPolicy = new TimedRotationPolicy(60.0f, TimedRotationPolicy.TimeUnit.SECONDS);
+        FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(100.0f, FileSizeRotationPolicy.Units.MB);
 
         FileNameFormat fileNameFormat = new DefaultFileNameFormat()
                 .withPath("/tmp/foo/")
-                .withExtension(".txt");
+                .withExtension(".log");
 
         // use "|" instead of "," for field delimiter
         RecordFormat format = new DelimitedRecordFormat()
                 .withFieldDelimiter("|");
 
-//        Yaml yaml = new Yaml();
-//        InputStream in = new FileInputStream(args[1]);
-//        Map<String, Object> yamlConf = (Map<String, Object>) yaml.load(in);
-//        in.close();
 
         Config config = new Config();
-        config.setNumWorkers(1);
-//        config.put("hdfs.config", yamlConf);
+        config.setNumWorkers(3);
 
+        KafkaSpoutConfig spoutConf =  KafkaSpoutConfig.builder("ip-10-28-124-152:6667", "benchmark-3-20")
+                .setGroupId("kafka-storm-test")
+                .setOffsetCommitPeriodMs(1000)
+                .setFirstPollOffsetStrategy(UNCOMMITTED_LATEST)
+                .setMaxUncommittedOffsets(1000000)
+                .setRecordTranslator(new TupleBuilder(), new Fields("id","uuid"))
+                .build();
 
         HdfsBolt bolt = new HdfsBolt()
-                .withFsUrl("hdfs://cdh01:8020")
+                .withFsUrl("hdfs://ip-10-28-124-152:8020")
                 .withFileNameFormat(fileNameFormat)
                 .withRecordFormat(format)
                 .withRotationPolicy(rotationPolicy)
@@ -57,15 +62,20 @@ public class HDFSTopology {
                 .addRotationAction(new MoveFileAction().toDestination("/tmp/dest2/"));
 
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("hdfs-spout", new HDFSSpout(), 1);
-        builder.setBolt("hdfs-bolt", bolt, 4).shuffleGrouping("hdfs-spout");
+//        builder.setSpout("hdfs-spout", new HDFSSpout(), 3);
+        builder.setSpout("kafka-spout", new KafkaSpout(spoutConf), 6);
+        builder.setBolt("hdfs-bolt", bolt, 4).shuffleGrouping("kafka-spout");
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("hdfs-test-topology", config, builder.
+        StormSubmitter.submitTopology("mytopology", config, builder.
                 createTopology());
 
-        Thread.sleep(100* 1000);
-        cluster.killTopology("hdfs-test-topology");
-        cluster.shutdown();
+    }
+}
+
+class TupleBuilder implements Func<ConsumerRecord<String, String>, List<Object>> {
+
+    public List<Object> apply(ConsumerRecord<String, String> record) {
+        Object records[] = record.value().split("\\|");
+        return Arrays.asList(records);
     }
 }
